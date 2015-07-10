3d87a6ba69ba73e4efee47c85633f233917bec49
Asko Soukka <asko.soukka@iki.fi>
2015-07-10 11:34:41 +0300

    Back to development: 0.6.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='collective.recipe.nix',
-    version='0.6.0',
+    version='0.6.1.dev0',
     description="Buildout recipe for creating Nix expressions from eggs list",
     long_description=(open('README.rst').read() + '\n' +
                       open('CHANGES.rst').read()),
