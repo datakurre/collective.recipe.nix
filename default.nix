d6589af571255ba6d4705a9e55084dd8bdf9d230
Asko Soukka <asko.soukka@iki.fi>
2015-07-28 14:45:45 +0300

    Update build environment

--- default.nix
+++ default.nix
@@ -1,15 +1,15 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation {
-  name = "myEnv";
+  name = "build";
   buildInputs = [
     libxml2
     libxslt
     pythonPackages.ldap
     pythonPackages.pillow
     pythonPackages.readline
-    pythonPackages.zc_buildout_nix
+    (pythonPackages.zc_buildout_nix.overrideDerivation (args: { postInstall = ""; }))
   ];
   shellHook = ''
-    export SSL_CERT_FILE=~/.nix-profile/etc/ca-bundle.crt
+    export SSL_CERT_FILE=${cacert}/etc/ssl/certs/ca-bundle.crt
   '';
 }
